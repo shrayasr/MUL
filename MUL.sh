baf914601eef82cad2f47d46b6aae9f47667de94
Shrayas <shrayasr@gmail.com>
2012-03-25 20:11:54 +0530

    listing , deleting and initializing done !

--- MUL.sh
+++ MUL.sh
@@ -3,6 +3,7 @@
 tflag=
 lflag=
 dflag=
+iflag=
 
 while [ $# -gt 0 ]
 do
@@ -14,18 +15,27 @@ do
 			shift
 			shift
 			break;;
-		-l) lflag=on;;
+
+		-l) lflag=on
+			break;;
+
 		-d)	dflag=on
 			manga=$2
 			shift
+			break;;
+
+		-i)	iflag=on
 			break;;
 	esac
 
 	shift
 done
 
-if [ tflag ]
+#echo $tflag "-" $lflag "-" $dflag "-" $iflag
+
+if [ $tflag ]
 then
+
 	code=`curl -s -L -w %{http_code} -o /dev/null www.mangareader.net/$manga`
 	
 	if [ $code == "404" ]
@@ -39,6 +49,35 @@ then
 		touch mangas
 	fi
 
-	echo $manga >> mangas
+	echo "$manga|$lastChapter" >> mangas
 
+	echo "Gotcha! Manga: $manga | Last chapter read: $lastChapter"
+
+elif [ $lflag ]
+then
+
+	cat mangas | while read LINE
+	do
+		manga=`echo $LINE | cut -d "|" -f1`
+		lastChapter=`echo $LINE | cut -d "|" -f2`
+		echo $manga ":" $lastChapter
+	done
+
+elif [ $dflag ]
+then
+
+	mv mangas mangas.old
+	awk -v rem=$manga 'match($1,rem) == 0 {print $0}' mangas.old > mangas
+	rm mangas.old
+
+elif [ $iflag ]
+then
+
+	if [ -f mangas ]
+	then
+		rm mangas
+	fi
+
+else
+	echo "BOO"
 fi
