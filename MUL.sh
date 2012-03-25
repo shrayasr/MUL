73770cd013425b09280f741757ff07810af49b38
Shrayas <shrayasr@gmail.com>
2012-03-25 18:58:00 +0530

    handling commandline args the right way ! WOO. Thanks http://www.shelldorado.com/goodcoding/cmdargs.html

--- MUL.sh
+++ MUL.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+tflag=off
+lflag=off
+dflag=off
+
+while [ $# -gt 0 ]
+do
+	case "$1" in
+		-t)	tflag=on
+			manga=$2
+			lastChapter=$3
+			shift
+			shift
+			break;;
+		-l) lflag=on;;
+		-d)	dflag=on
+			manga=$2
+			shift
+			break;;
+	esac
+
+	shift
+done
